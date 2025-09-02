f6bc63cb9813344dbcf6ea0884ee33ad6d73afb9
James Traylor <chariot.rome@outlook.com>
2025-09-02 19:59:20 -0400

    Update inventory_app.py

--- inventory_app.py
+++ inventory_app.py
@@ -49,19 +49,14 @@ def init_mongodb():
         st.stop()
 
 # Initialize MongoDB collection
- try:
- inventory_collection = init_mongodb()
- #   if inventory_collection is not None:
-  #      msg = st.empty()  # placeholder
-   #     msg.success("Connected to MongoDB Atlas successfully!")
-    #    time.sleep(.5)
-      #  msg.empty()  # clears the message
-#except:
-  #  msg = st.empty()
-  #  msg.error("Database connection failed. Please check your setup.")
- #   time.sleep(.5)
-#    msg.empty()
- #   st.stop()
+ try: 
+     inventory_collection = init_mongodb()
+     if ineventory_collection is not None:
+         st.success()
+         st.stop
+ except:
+     st.error("DB Connection Failed")
+     st.stop()
 
 # --- Enhanced Helper Functions ---
 def add_item(brand, name, pid, min_qty, current_amount, per_package, per_box, per_case, cost, last_checked):
@@ -1162,6 +1157,7 @@ with tab2:
 
 # Footer
 st.divider()
+
 
 
 
